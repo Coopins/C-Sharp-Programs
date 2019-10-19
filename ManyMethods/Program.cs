15d942f2b62b21debad5f055f937c18f06a69a70
Chris <coopertime08@gmail.com>
2019-10-19 17:28:19 -0500

    Initial commit

---
+++ ManyMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    //my first program to display the words Hello World in the console!
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+
+        static void hello (string[] args)
+        {
+            string firstname = "what's your name?"
+            Console.WriteLine("What's your name?");
+            Console.Read();
+        }
+    }
+}
