a3b3d23982b906d252773c93cb4af96598234aad
Chris <coopertime08@gmail.com>
2019-11-17 20:59:22 -0600

    SuperHeroes/

--- SuperHeroes/Program.cs
+++ SuperHeroes/Program.cs
@@ -1,12 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SuperHeroes
 {
     class Program
     {
+        static List<Person> Humans = new List<Person>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Humans.Add(new Person("Hannah Banana", "Bank CEO"));
+            Humans.Add(new Person("Davis #2", "Homeowner"));
+            Humans.Add(new SuperHero("Monkey D. Luffy", "Body made of rubber"));
+            Humans.Add(new Villian("Black Beard", "Can manipulate darkness"));
+
+            foreach (Person person in Humans)
+            {
+                person.PrintGreeting();
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+
+        public Person(string name, string nickName)
+
+        {
+            Name = name;
+            Nickname = nickName;
+        }
+
+        public override string ToString()
+
+        {
+            return Name;
+        }
+
+        public virtual void PrintGreeting()
+        {
+            Console.Write(Name + ":");
+            Console.WriteLine(" Hi, My name is {0}, you can call me {1}", Name, Nickname);
+
+        }
+
+        class SuperHero : Person
+        {
+            public string RealName { get; set; }
+            public string SuperPower { get; set; }
+
+            public SuperHero(string name, string realName, string superPower) : base(name, null)
+            {
+                RealName = realName;
+                SuperPower = superPower;
+
+            }
+
+            public override void PrintGreeting()
+            {
+                Console.WriteLine(Name + ":");
+                Console.WriteLine(" I am {0}. when I am {1}, my super power" +
+                    "is {2}", RealName, Name, SuperPower);
+            }
+
+        }
+
+        class Villian : Person
+        {
+            public String Nemesis;
+
+            public Villian(string name, string nemesis) : base(name, null)
+            {
+                Nemesis = nemesis;
+            }
+
+            public override void PrintGreeting()
+            {
+
+                Console.Write(Name + ":");
+                Console.WriteLine(" I am {0}! have you seen {1}?", Name, Nemesis);
+            }
+
+        }
+
     }
 }
