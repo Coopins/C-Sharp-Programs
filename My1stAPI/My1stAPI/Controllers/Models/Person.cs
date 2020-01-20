c977fd9ce8ec6a8721a9e4a3802bd3e958160308
Chris <coopertime08@gmail.com>
2020-01-19 18:48:49 -0600

    all

---
+++ My1stAPI/My1stAPI/Controllers/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My1stAPI.Controllers.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public int Id { get; set; }
+            public string Name { get; set; }
+            public string HairColor { get; set; }
+
+    }
+}
--- MyFirstAPI/README.md
+++
@@ -1,2 +0,0 @@
-# MyFirstAPI
-API learning and practice (LCA)
